d9eaadc64011e9a2b180f6a968ad5244d1bb5e62
Beto-Beto-Salamander <ryan.octavius.saputra@gmail.com>
2023-01-04 15:00:05 +0700

    update function

--- public/js/set-local.js
+++ public/js/set-local.js
@@ -1,5 +1,7 @@
 //bikin fungsi set local storage
 //params - key & value
-function CustomSetLocalStorage(key, value){
-   localStorage.setItem(key, value);
+function CustomSetLocalStorage(){
+   localStorage.setItem(Math.random().toString(36), Math.random().toString(36));
 }
+
+CustomSetLocalStorage();
