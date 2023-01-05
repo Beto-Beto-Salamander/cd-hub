ca696ea9c4827e95b55fa4b995abb7208aeca185
Beto-Beto-Salamander <ryan.octavius.saputra@gmail.com>
2023-01-05 12:16:19 +0700

    update custom local storage

--- public/js/custom-local-storage.js
+++ public/js/custom-local-storage.js
@@ -1,35 +1,31 @@
-var CustomLocalStorage = {};
-
-CustomLocalStorage.set = function (key, value) {
-    window.localStorage.setItem(key, value);
-}
-
-CustomLocalStorage.getByKeys = function (keys) {
-    var storage, result, value;
-
-    storage = window.localStorage;
-    result = [];
-
-    for (var i = 0; i < keys.length; i++) {
-        try {
-            value = storage.getItem(keys[i]);
-        } catch (e) {
-            value = null;
+var CustomLocalStorage = /** @class */ (function () {
+    function CustomLocalStorage() {
+    }
+    CustomLocalStorage.prototype.set = function (key, value) {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
+    CustomLocalStorage.prototype.getByKeys = function (keys) {
+        var storage, result, value;
+        storage = window.localStorage;
+        result = [];
+        for (var i = 0; i < keys.length; i++) {
+            try {
+                value = storage.getItem(keys[i]);
+            }
+            catch (e) {
+                value = null;
+            }
+            result.push(value);
         }
-
-        result.push(value);
-    }
-
-    return (result.length > 1) ? result : result[0];
-
-}
-
-CustomLocalStorage.removeByKeys = function (keys) {
-    for (var i = 0; i < keys.length; i++) {
-        window.localStorage.removeItem(keys)
-    }
-}
-
-CustomLocalStorage.removeAll = function () {
-    window.localStorage.clear();
-}
+        return (result.length > 1) ? result : result[0];
+    };
+    CustomLocalStorage.prototype.removeByKeys = function (keys) {
+        for (var i = 0; i < keys.length; i++) {
+            window.localStorage.removeItem(keys[i]);
+        }
+    };
+    CustomLocalStorage.prototype.removeAll = function () {
+        window.localStorage.clear();
+    };
+    return CustomLocalStorage;
+}());
