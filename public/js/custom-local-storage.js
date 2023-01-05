c0f52f790a4288e99e20160f17ceac845f97512d
Beto-Beto-Salamander <ryan.octavius.saputra@gmail.com>
2023-01-05 14:40:32 +0700

    change ts class name

--- public/js/custom-local-storage.js
+++ public/js/custom-local-storage.js
@@ -1,31 +1,31 @@
-var CustomLocalStorage = /** @class */ (function () {
-    function CustomLocalStorage() {
+var LocalStorage = /** @class */ (function () {
+    function LocalStorage() {
+        this.storage = window.localStorage;
     }
-    CustomLocalStorage.prototype.set = function (key, value) {
-        window.localStorage.setItem(key, JSON.stringify(value));
+    LocalStorage.prototype.set = function (key, value) {
+        this.storage.setItem(key, JSON.stringify(value));
     };
-    CustomLocalStorage.prototype.getByKeys = function (keys) {
-        var storage, result, value;
-        storage = window.localStorage;
+    LocalStorage.prototype.getByKeys = function (keys) {
+        var result, value;
         result = [];
         for (var i = 0; i < keys.length; i++) {
             try {
-                value = storage.getItem(keys[i]);
+                value = this.storage.getItem(keys[i]);
             }
             catch (e) {
                 value = null;
             }
             result.push(value);
         }
-        return (result.length > 1) ? result : result[0];
+        return result.length > 1 ? result : result[0];
     };
-    CustomLocalStorage.prototype.removeByKeys = function (keys) {
+    LocalStorage.prototype.removeByKeys = function (keys) {
         for (var i = 0; i < keys.length; i++) {
-            window.localStorage.removeItem(keys[i]);
+            this.storage.removeItem(keys[i]);
         }
     };
-    CustomLocalStorage.prototype.removeAll = function () {
-        window.localStorage.clear();
+    LocalStorage.prototype.removeAll = function () {
+        this.storage.clear();
     };
-    return CustomLocalStorage;
+    return LocalStorage;
 }());
